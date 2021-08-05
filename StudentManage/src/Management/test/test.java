7214afa474d30969d76cc0fae7da2534220308f4
DQ <1259337898@qq.com>
2021-08-05 16:19:53 +0800

    commit 1

--- StudentManage/src/Management/test/test.java
+++ StudentManage/src/Management/test/test.java
@@ -6,7 +6,6 @@ import Management.Pojo.Student;
 import Management.Service.Impl.StudentServiceImpl;
 import Management.Service.StudentService;
 import Management.Utils.JdbcUtils;
-import org.junit.Test;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -16,24 +15,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class test {
 
 
-    @Test
-    public void test01(){
-        StudentService studentService = new StudentServiceImpl();
-        Student student = studentService.queryById("1910300723");
-        //Student student1 = new Student("1910300111","黄晓宇","男","材料与化学");
-
-        //studentService.save(student1);
-
-        //studentService.updateNameById("1910300723","胡婧禾");
-
-//        studentService.updateSchoolById("1910300723","经济与管理");
-        System.out.println(student);
-    }
-
-    @Test
-    public void test02(){
-
-    }
+   
 
 
 }
